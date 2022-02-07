b26333237de4c29d39876d4842cd9599006d42f4
arguxx <argianradityaputra21@gmail.com>
2022-02-07 21:18:45 +0700

    adding static files

--- polls/views.py
+++ polls/views.py
@@ -13,8 +13,10 @@ class IndexView(generic.ListView):
         """Return the last five published questions."""
         return Question.objects.order_by('-pub_date')[:5]
 
+
 class DetailView(generic.DetailView):
-    ...
+    model = Question
+    template_name = 'polls/detail.html'
     def get_queryset(self):
         """
         Excludes any questions that aren't published yet.
