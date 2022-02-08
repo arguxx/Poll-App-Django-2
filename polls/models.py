d8f6e43d7c9321242c8ba563a035fa08dae67fd5
arguxx <argianradityaputra21@gmail.com>
2022-02-08 09:25:13 +0700

    improve models and views

--- polls/models.py
+++ polls/models.py
@@ -10,13 +10,9 @@ class Question(models.Model):
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
-        
-    
-    
+
     def __str__(self):
         return self.question_text
-
-
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
